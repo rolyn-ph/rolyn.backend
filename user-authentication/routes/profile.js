939152cfeeaacb292545186c15d5f5ee3fc193a8
MiggyV23 <migueljvillanueva1@gmail.com>
2024-08-23 11:01:46 +0800

    Fixed typeError bug

--- user-authentication/routes/profile.js
+++ user-authentication/routes/profile.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { verifyToken } = require('../middleware/auth');
+const verifyToken = require('./auth');
 const User = require('../models/User');
 
 const router = express.Router();
